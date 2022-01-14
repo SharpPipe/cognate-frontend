d7a19604346e96bf0f884178188479b6cda0ee72
Mart Kaasik <mart.kaasik@gmail.com>
2022-01-14 21:34:25 +0200

    New home screen

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <Navbar/>
+
+    <div class="container px-4 px-lg-5 d-flex my-auto h-100 align-items-center justify-content-center">
+
+      <div class="jumbotron">
+        <h1 class="display-1 font-weight-lighter font-italic">Cognate</h1>
+        <p class="lead">Some people still know how learning is monitored.</p>
+        <hr class="my-4">
+        <p>Using modular dynamics and the common spectral analysis we are able to not only predict put alter the course of the success of a project.</p>
+        <a class="btn btn-info btn-lg" href="#" role="button">Learn more</a>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+
+export default {
+  name: 'Home',
+  data() {
+    return {}
+  },
+  components: {
+    Navbar,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
