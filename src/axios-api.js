2320f05930dc8ce31dcf3ff4f9ba50487487d954
Mart Kaasik <mart.kaasik@gmail.com>
2022-01-25 18:56:21 +0200

    Rename APIData to just Api

--- src/axios-api.js
+++ src/axios-api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-const getAPI = axios.create({
+const Api = axios.create({
     baseURL: 'http://35.228.100.147:8000/',
     timeout: 1000,
 })
 
-export { getAPI }
+export { Api }
