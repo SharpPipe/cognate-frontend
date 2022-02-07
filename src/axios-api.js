f47e93c8f4d5f2f8e496635e500933defde844e1
Mart Kaasik <mart.kaasik@gmail.com>
2022-02-07 20:47:24 +0200

    13 Token is refreshed automatically after 401

--- src/axios-api.js
+++ src/axios-api.js
@@ -1,13 +1,44 @@
 import axios from 'axios'
 import store from './store'
+import router from './routes'
 
 const Api = axios.create({
     baseURL: 'http://193.40.156.179:8081/',
     timeout: 1000,
     headers: {
-        'Authorization': `Bearer ${store.state.accessToken}`,
-        'Content-type':  'application/json'
+        'Content-type': 'application/json'
     }
 })
+
+// Auto-refresh code from
+// https://lewiskori.com/blog/how-to-auto-refresh-jwts-using-axios-interceptors/
+Api.interceptors.request.use(
+    (config) => {
+        if (store.state.accessToken)
+            config.headers.Authorization = `Bearer ${store.state.accessToken}`
+        return config
+    },
+    error => Promise.reject(error)
+)
+
+Api.interceptors.response.use(
+    response => response,
+    async error => {
+        const originalRequest = error.config;
+        if (
+            error.response.status === 401 &&  // Unauthorized
+            originalRequest.url.includes("api/token/refresh/")
+        ) {
+            store.commit("destroyToken");
+            router.push({name: 'home'});
+            return Promise.reject(error);
+        } else if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            await store.dispatch("refreshToken");
+            return axios(originalRequest);
+        }
+        return Promise.reject(error);
+    }
+)
 
 export { Api }
