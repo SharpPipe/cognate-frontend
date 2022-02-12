f51f8eb264e0eb174f7b42e6d8f19f66d1267af9
mart <makaa@ttu.ee>
2022-02-12 22:39:39 +0200

    10 Add input fields

--- src/components/visualizations/CourseGradingTreeLinear.vue
+++ src/components/visualizations/CourseGradingTreeLinear.vue
@@ -5,26 +5,89 @@
         <input
           type="text"
           class="form-control"
-          placeholder="New grade element"
+          placeholder="New grade element with points worth"
           aria-label="Grade element"
           aria-describedby="button-addon1"
-          v-model="newNode.name"
+          v-model="payload.name"
+        />
+        <select
+          required
+          class="form-control my-0 col-sm-2"
+          v-model="payload.grade_type"
+        >
+          <option value="" selected disabled hidden>Grade Type</option>
+          <option value="C">Custom</option>
+          <option value="S">Sum</option>
+          <option value="I">Min</option>
+          <option value="M">Max</option>
+        </select>
+        <input
+          type="number"
+          class="form-control col-sm-1"
+          placeholder="points worth"
+          aria-label="points worth"
+          aria-describedby="button-addon-points"
+          v-model="payload.total"
         />
         <div class="input-group-append">
           <button
             class="btn btn-outline-success"
             type="button"
             id="button-addon2"
+            @click="addNode"
           >
-            Add Child
+            Add Component
           </button>
         </div>
+      </div>
+      <div class="input-group mb-2">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Description"
+          aria-label="description"
+          aria-describedby="description"
+          v-model="payload.description"
+        />
+      </div>
+      <div class="input-group mb-2">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            Is Milestone? &nbsp;
+            <input
+              id="ms_check"
+              type="checkbox"
+              @change="toggleMilestone"
+              aria-label="Checkbox for following text input"
+            />
+          </div>
+        </div>
+        <date-picker
+          id="starttime"
+          v-model="payload.start"
+          disabled
+          :config="options"
+        ></date-picker>
+        <div class="input-group-text rounded-0 mx-n1">to</div>
+        <date-picker
+          id="endtime"
+          disabled
+          v-model="payload.end"
+          :config="options"
+        ></date-picker>
       </div>
       <div class="input-group d-flex">
         <div class="input-group-prepend flex-grow-1">
           <label class="input-group-text flex-grow-1" for="inputGroupSelect01">
             Selected: &nbsp;
-            <em class="font-weight-bold">{{ currentNode.data.name }} </em>
+            <em class="font-weight-bold">{{ currentNode.data.name, }} </em>
+          </label>
+          <label
+            class="input-group-text flex-shrink-1"
+            for="inputGroupSelect01"
+          >
+            Points worth: &nbsp;
+            <em class="font-weight-bold">{{ currentNode.data.total, }} </em>
           </label>
         </div>
         <div class="input-group-append w-20">
@@ -32,6 +95,7 @@
             class="btn btn-outline-danger"
             type="button"
             id="button-addon2"
+            @click="deleteNode"
           >
             Delete Node
           </button>
@@ -43,61 +107,127 @@
 
 <script>
 import * as d3 from "d3";
+import { Api } from "../../axios-api";
+import { mapState } from "vuex";
+// https://github.com/ankurk91/vue-bootstrap-datetimepicker
+import datePicker from "vue-bootstrap-datetimepicker";
+import "pc-bootstrap4-datetimepicker/build/css/bootstrap-datetimepicker.css";
 const width = 932;
 const height = 932;
 let data;
 
 export default {
   name: "CourseGradingTree",
+  props: ["gradedata"],
+  computed: mapState(["APIData"]),
+  components: {
+    datePicker,
+  },
   data() {
     return {
+      date: new Date(),
+      options: {
+        format: "YYYY-MM-DD",
+        useCurrent: false,
+        locale: "en-gb",
+      },
       width,
       height,
       data,
       dataLoaded: false,
       currentNode: {
         id: null,
-        data: { name: "" },
+        data: { name: "", id: null, total: 0 },
       },
-      newNode: {
-        id: 0,
+      payload: {
         name: "",
+        total: 5,
+        grade_type: "",
+        start: "",
+        end: "",
+        description: "",
       },
+      graph: null,
+      rootId: null,
     };
   },
   created() {
-    //d3.json("/gradingmockdata2.json")
-    d3.json("/games.json").then((d) => {
+    /*     d3.json("/games.json").then((d) => {
       this.data = d;
       this.dataLoaded = true;
-    });
+    }); */
   },
   mounted() {
-    this.renderGraph();
+    this.data = this.gradedata;
+    function refactor(data = null) {
+      return data;
+    }
+    this.rootId = this.gradedata.id;
+    this.data = refactor(this.gradedata);
+
+    this.dataLoaded = true;
+    //this.renderGraph();
   },
   watch: {
     dataLoaded() {
-      if (!this.dataLoaded) return;
-      console.log("data loaded");
       this.renderGraph();
-      console.log("graph rendered");
     },
   },
   methods: {
+    toggleMilestone() {
+      let check = document.getElementById("ms_check").checked;
+      let start = document.getElementById("starttime");
+      let end = document.getElementById("endtime");
+      if (check) {
+        start.disabled = false;
+        end.disabled = false;
+      } else {
+        start.disabled = true;
+        end.disabled = true
+        start.value = "";
+        end.value = ""
+        this.payload.start = ""
+        this.payload.end = ""
+        this.payload.grademilestone = false
+      }
+    },
+    addNode() {
+      console.log(this.payload);
+      if (this.payload.name && this.currentNode.data.name) {
+        Api.post(
+          "grade_category/" + this.currentNode.data.id + "/",
+          this.payload
+        )
+          .then(() => {
+            this.$router.go();
+          })
+          .catch((err) => console.log(err));
+      }
+    },
+    deleteNode() {
+      if (this.currentNode.data.id !== this.gradedata.id) {
+        // don't delete the root, please
+        Api.delete("grade_category/" + this.currentNode.data.id + "/")
+          .then(() => {
+            this.currentNode = null;
+            this.$router.go();
+          })
+          .catch((err) => console.log(err));
+      }
+    },
     renderGraph() {
-      // Git Log code inspired by and definitely not directly stolen from
-      // https://observablehq.com/@aaronkyle/interactive-tree-diagram-d3v3
+      // Tidy Tree code inspired by and definitely not directly stolen from
       // https://observablehq.com/@asktree/interactive-tree-diagram-d3v4-v5
-      // https://observablehq.com/@esperanc/d3-radial-tidy-tree
+      // https://observablehq.com/@jacob-tu/collapsible-tree
       if (!this.dataLoaded) return;
       //let reftothis = this
       data = this.data;
 
       let self = this;
 
-      let dx = 24;
+      let dx = 30;
       let dy = width / 6;
-      let margin = { top: 10, right: 120, bottom: 10, left: 80 };
+      let margin = { top: 20, right: 120, bottom: 20, left: 80 };
 
       let tree = d3.tree().nodeSize([dx, dy]);
       let diagonal = d3
@@ -113,7 +243,7 @@ export default {
       root.descendants().forEach((d, i) => {
         d.id = i;
         d._children = d.children;
-        if (d.depth && d.data.name.length !== 7) d.children = null;
+        if (d.depth && d.data.name.length === -1) d.children = null;
       });
 
       const svg = d3
@@ -172,20 +302,31 @@ export default {
           .attr("stroke-opacity", 0)
           .on("click", (event, d) => {
             self.currentNode.data.name = d.data.name;
+            self.currentNode.data.id = d.data.id;
+            self.currentNode.data.total = d.data.total;
+            console.log(d.data);
             d.children = d.children ? null : d._children;
             update(d);
           });
 
         nodeEnter
           .append("circle")
-          .attr("r", 10)
+          .attr("r", 12)
           .attr("fill", (d) => (d._children ? "#444" : "#aaa"))
           .attr("stroke-width", 10);
 
         nodeEnter
           .append("text")
           .attr("dy", "0.31em")
-          .attr("x", (d) => (d._children ? -15 : 15))
+          .attr("x", 0)
+          .attr("text-anchor", "middle")
+          .style("font", "10px sans-serif")
+          .text((d) => Math.round(d.data.total));
+
+        nodeEnter
+          .append("text")
+          .attr("dy", "0.31em")
+          .attr("x", (d) => (d._children ? -18 : 18))
           .attr("text-anchor", (d) => (d._children ? "end" : "start"))
           .text((d) => d.data.name)
           .clone(true)
@@ -254,4 +395,8 @@ export default {
 </script>
 
 <style >
+select:invalid,
+select option[value=""] {
+  color: #6c757d;
+}
 </style>
--- src/views/Grading.vue
+++ src/views/Grading.vue
@@ -1,21 +1,34 @@
 <template>
   <div class="milestone">
     <div class="container">
-      <CourseGradingTree />
+      <h2>Grading for the Course {{$route.params.name}}</h2>
+      <CourseGradingTree v-if="APIData" :gradedata="APIData"/>
     </div>
   </div>
 </template>
 
 <script>
 import CourseGradingTree from "../components/visualizations/CourseGradingTreeLinear.vue";
+import { Api } from "../axios-api";
+import { mapState } from 'vuex'
 
 export default {
   name: "Grading",
   components: {
     CourseGradingTree,
   },
+  computed: mapState(['APIData']),
   data() {
     return {};
+  },
+  created() {
+    Api.get("groups/" + this.$route.params.id + "/grading/")
+      .then((response) => {
+        this.$store.state.APIData = response.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
 };
 </script>
