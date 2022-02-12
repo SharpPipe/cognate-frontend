19b587c158ec088ad64fc9f7eac6fb7046aeb3a3
mart <makaa@ttu.ee>
2022-02-12 10:32:59 +0200

    10 Delete bloat

--- src/views/Grading.vue
+++ src/views/Grading.vue
@@ -18,29 +18,6 @@ export default {
   },
   data() {
     return {
-      APIData: {
-        'project_name': 'Minecraft',
-        'milestone_name': 'First Milestone',
-        'developers': [
-          { 'name': 'priit', 'status': 0, 'time_spent': 5 },
-          { 'name': 'tiit', 'status': 0, 'time_spent': 24 },
-          { 'name': 'viive', 'status': 1, 'time_spent': 9 }
-        ],
-        'milestone_stats': {
-          'total_time_spent': 33,
-          'total_lines_added': 133,
-          'total_lines_removed': 43,
-          'total_issues_closed': 5,
-          'total_fun': 66,
-        },
-        'prev_milestone_stats': {
-          'total_time_spent': 27,
-          'total_lines_added': 653,
-          'total_lines_removed': 3,
-          'total_issues_closed': 8,
-          'total_fun': 60,
-        },
-      }
     }
   },
 }
