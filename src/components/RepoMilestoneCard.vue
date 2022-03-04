c7de536f623bbd457e55af7bf562e359860130af
Mart Kaasik <mart.kaasik@gmail.com>
2022-03-04 18:40:54 +0200

    #30 Can grade all 7 milestones

--- src/components/RepoMilestoneCard.vue
+++ src/components/RepoMilestoneCard.vue
@@ -4,9 +4,9 @@
             <label v-if="ms_data">{{ms_data.title}}</label>
             <label v-if="!ms_data">Milestone {{ nr }}</label>
             <br>
-            <div v-if="ms_status =='graded'" class="badge badge-pill badge-success">Graded</div>
+<!--             <div v-if="ms_status =='graded'" class="badge badge-pill badge-success">Graded</div>
             <div v-if="ms_status =='ungraded'" class="badge badge-pill badge-warning">Not Graded</div>
-            <div v-if="ms_status =='TBA'" class="badge badge-pill badge-dark milestone">TBA</div>
+            <div v-if="ms_status =='TBA'" class="badge badge-pill badge-dark milestone">TBA</div> -->
             <h3>{{points}}</h3>
         </div>
     </div>
