3ced3c25357bd50fb37af6f364ac2653d1ead3ba
mart <makaa@ttu.ee>
2022-04-14 14:17:56 +0300

    #58 Hover on dev working

--- src/views/Repo.vue
+++ src/views/Repo.vue
@@ -2,7 +2,10 @@
   <div class="repo">
     <div v-if="projectDetails" class="container">
       <!--  GitLab links  -->
-      <h5 v-for="gitlabrepo in projectDetails.repositories" :key="gitlabrepo.gitlab_id">
+      <h5
+        v-for="gitlabrepo in projectDetails.repositories"
+        :key="gitlabrepo.gitlab_id"
+      >
         <a :href="gitlabrepo.url" target="_blank">
           <font-awesome-icon icon="fa-brands fa-gitlab" />GitLab:
           <em>{{ gitlabrepo.name }}</em>
@@ -12,7 +15,7 @@
       <!--  Overview data  -->
       <div class="row m-1">
         <div class="col-4 p-0" v-if="radar.length">
-          <RepoRadar class="p-1"  :radardata="radar" />
+          <RepoRadar class="p-1" :radardata="radar" />
         </div>
 
         <div class="col-5 p-0 d-flex flex-column align-content-stretch">
@@ -22,7 +25,11 @@
               :key="dev.username"
               class="d-flex col m-0"
             >
-              <RepoDeveloper :devData="dev" class="w-100 m-1" />
+              <RepoDeveloper
+                :devData="dev"
+                class="w-100 m-1"
+                @mouseover.native="enterDev(dev)"
+              />
             </div>
           </div>
         </div>
@@ -49,7 +56,7 @@
                   msid: milestone.milestone_id,
                   start: milestone.start_time,
                   end: milestone.end_time,
-                }
+                },
               }"
             >
               <RepoMilestoneCard class="m-0" :msData="milestone" />
@@ -60,7 +67,11 @@
 
       <!--  Graph  -->
       <div class="row" v-if="gittimedata.length && devColours.loaded">
-        <GitTime class="w-100" :gitdata="gittimedata" :colours="devColours.users" />
+        <GitTime
+          class="w-100"
+          :gitdata="gittimedata"
+          :colours="devColours.users"
+        />
       </div>
 
       <!--  Comments  -->
@@ -88,13 +99,13 @@ import RepoMilestoneCard from "../components/RepoMilestoneCard.vue";
 
 import { Api } from "../axios-api";
 export default {
-  name: 'Repo',
+  name: "Repo",
   components: {
     GitTime,
     RepoRadar,
     RepoDeveloper,
     RepoTotalStats,
-    RepoMilestoneCard
+    RepoMilestoneCard,
   },
   data() {
     return {
@@ -105,7 +116,7 @@ export default {
           { axis: "Branches", value: 2 },
           { axis: "Planning", value: 2 },
           { axis: "Issues", value: 1 },
-        ]
+        ],
       ],
       currentPoints: 30,
       minCoursePoints: 0,
@@ -114,7 +125,10 @@ export default {
       gradeMilestones: null,
       issueData: [],
 
-      projectTimeRange: [new Date(2022, 0, 24, 0, 0, 0), new Date(2022, 7, 16, 0, 0, 0)],
+      projectTimeRange: [
+        new Date(2022, 0, 24, 0, 0, 0),
+        new Date(2022, 7, 16, 0, 0, 0),
+      ],
       comments: [],
       projectDetails: [],
 
@@ -122,90 +136,92 @@ export default {
       gittimedata: [],
       devColours: {
         loaded: false,
-        users: {}
+        users: {},
       },
-    }
+    };
   },
   created() {
-    const repoid = this.$route.params.repoid
+    const repoid = this.$route.params.repoid;
     Api.get("/projects/" + repoid + "/")
-      .then(response => {
-        this.projectDetails = response.data
-        for (let dev of this.projectDetails.developers) this.devColours["users"][dev.username] = dev.colour
-        this.devColours.loaded = true
+      .then((response) => {
+        this.projectDetails = response.data;
+        for (let dev of this.projectDetails.developers)
+          this.devColours["users"][dev.username] = dev.colour;
+        this.devColours.loaded = true;
 
-        this.radar = this.formatMilestonePointsForRadarGraph(this.projectDetails.milestones)
-
+        this.radar = this.formatMilestonePointsForRadarGraph(
+          this.projectDetails.milestones
+        );
       })
-      .catch(err => {
-        console.log(err)
-      })
+      .catch((err) => {
+        console.log(err);
+      });
 
     Api.get("/feedback/", { params: { type: "PA", project: repoid } })
-      .then(response => {
-        this.comments = response.data
+      .then((response) => {
+        this.comments = response.data;
       })
-      .catch(err => {
-        console.log(err)
-      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-
-    Api.get('/projects/' + repoid + "/time_spent/",
-      {
-        params: {
-          start: this.projectTimeRange[0].toISOString(),
-          end: this.projectTimeRange[1].toISOString()
-        }
+    Api.get("/projects/" + repoid + "/time_spent/", {
+      params: {
+        start: this.projectTimeRange[0].toISOString(),
+        end: this.projectTimeRange[1].toISOString(),
+      },
+    })
+      .then((response) => {
+        this.gittimedata = response.data;
       })
-      .then(response => {
-        this.gittimedata = response.data
-      })
-      .catch(err => {
-        console.log(err)
-      })
-
+      .catch((err) => {
+        console.log(err);
+      });
   },
   methods: {
     formatedTime(timeString) {
-      return new Date(timeString).toUTCString()
+      return new Date(timeString).toUTCString();
     },
     formatMilestonePointsForRadarGraph(milestones) {
-      // This is the result of Kristjan building bad APIs 
-      let numOfDevs = milestones[0].user_points.length
+      // This is the result of Kristjan building bad APIs
+      let numOfDevs = milestones[0].user_points.length;
       for (let ms of milestones) {
-        let r =
-          [
-            { axis: "Retro", value: 0 },
-            { axis: "Meeting", value: 0 },
-            { axis: "Branch management", value: 0 },
-            { axis: "Planning", value: 0 },
-            { axis: "Issues", value: 0 },
-          ]
+        let r = [
+          { axis: "Retro", value: 0 },
+          { axis: "Meeting", value: 0 },
+          { axis: "Branch management", value: 0 },
+          { axis: "Planning", value: 0 },
+          { axis: "Issues", value: 0 },
+        ];
         for (let user of ms.user_points) {
           for (let key in user.grades) {
             if (key !== "Effort") {
               if (key === "Meeting" || key === "Retro")
-                r.find(o => o.axis == key).value = user.grades[key]
+                r.find((o) => o.axis == key).value = user.grades[key];
               else
-                r.find(o => o.axis == key).value += user.grades[key] / numOfDevs
+                r.find((o) => o.axis == key).value +=
+                  user.grades[key] / numOfDevs;
             }
           }
         }
-        this.radar = this.radar.concat(Array(r))
-        r =
-          [
-            { axis: "Retro", value: 0 },
-            { axis: "Meeting", value: 0 },
-            { axis: "Branch management", value: 0 },
-            { axis: "Planning", value: 0 },
-            { axis: "Issues", value: 0 },
-          ]
+        this.radar = this.radar.concat(Array(r));
+        r = [
+          { axis: "Retro", value: 0 },
+          { axis: "Meeting", value: 0 },
+          { axis: "Branch management", value: 0 },
+          { axis: "Planning", value: 0 },
+          { axis: "Issues", value: 0 },
+        ];
       }
 
-      return this.radar
-    }
+      return this.radar;
+    },
+    // Hover features
+    enterDev(dev) {
+      console.log(dev.username);
+    },
   },
-}
+};
 </script>
 
 <style scoped>
