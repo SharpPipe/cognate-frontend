3b29427e38f41ba8abbc369d168cb937d318f9a8
mart <makaa@ttu.ee>
2022-04-14 19:45:03 +0300

    #51 Milestone Connection view done

---
+++ src/components/DragBoard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :id="id" class="board" @dragover.prevent @drop.prevent="drop">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { Api } from "../axios-api";
+
+export default {
+  props: ["id"],
+  methods: {
+    drop: (e) => {
+      const card_id = e.dataTransfer.getData("card_id");
+      const card = document.getElementById(card_id);
+      card.style.display = "block";
+      e.target.appendChild(card);
+
+      let gitlab_milestone_id = card.id.split("-")[1];
+      let cognate_milestone = e.target.id.split("-")[1];
+      if (cognate_milestone == "unmatched") cognate_milestone = null;
+
+      Api.put("/milestones/" + gitlab_milestone_id + "/grade_milestone/", {
+        id: cognate_milestone,
+      }).catch((error) => console.log(error));
+    },
+  },
+};
+</script>
--- src/views/RepoMilestoneManager.vue
+++ src/views/RepoMilestoneManager.vue
@@ -1,47 +1,124 @@
 <template>
-    <div class="container">
-        <h1>Manage Repo Milestone</h1>
+  <div v-if="milestone_connections" class="container">
+    <h1>Manage Repo Milestone</h1>
+    <div>
+      <div class="flexbox">
+        <div
+          class="border rounded p-0 my-2"
+          v-for="ms in milestone_connections.grade_milestones"
+          :key="ms.id"
+        >
+          <div class="w-25 p-3 my-2 float-left">
+            <div>
+              <div class="h3">Milestone {{ ms.milestone_order_id }}</div>
+              <div class="mr-1 badge badge-pill badge-secondary">
+                {{ ms.start.substring(0, 10) }}
+              </div>
+              <div class="badge badge-pill badge-secondary">
+                {{ ms.end.substring(0, 10) }}
+              </div>
+            </div>
+          </div>
+
+          <DragBoard class="p-2 w-75 float-left bg-dark" :id="`board-${ms.id}`">
+            <DragCard
+              :id="`card-${gitlab_ms.id}`"
+              draggable="true"
+              v-for="gitlab_ms in ms.gl_milestones"
+              :key="gitlab_ms.id"
+              class="justify-content-center bg-secondary px-4 py-2 m-3"
+            >
+              <font-awesome-icon class="h3" icon="fa-brands fa-gitlab" />
+              {{ gitlab_ms.title }}
+            </DragCard>
+          </DragBoard>
+        </div>
+
+        <!--  Unmatched  -->
+        <div
+          class="border rounded p-0 my-2"
+          v-for="unmatched in milestone_connections.unmatched"
+          :key="unmatched.id"
+        >
+          <div class="w-25 p-3 my-2 float-left">
+            <div class="h3">Unmatched</div>
+          </div>
+
+          <DragBoard class="p-2 w-75 float-left bg-dark" id="board-unmatched">
+            <DragCard
+              :id="`card-${unmatched.id}-unmatched`"
+              draggable="true"
+              class="justify-content-center bg-secondary px-4 py-2 m-3"
+            >
+              <font-awesome-icon class="h3" icon="fa-brands fa-gitlab" />
+              {{ unmatched.title }}
+            </DragCard>
+          </DragBoard>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import { Api } from "../axios-api";
+import DragBoard from "../components/DragBoard.vue";
+import DragCard from "../components/DragCard.vue";
 
 export default {
-    name: 'Groupadd',
-    data() {
-        return {
-            payload: {
-                type: "AP",
-                feedback: "",
-            },
+  name: "Groupadd",
+  components: {
+    DragBoard,
+    DragCard,
+  },
+  data() {
+    return {
+      milestone_connections: null,
+    };
+  },
+  created() {
+    Api.get(
+      "/projects/" + this.$route.params.repoid + "/milestone_connections/"
+    ).then((response) => {
+      this.milestone_connections = this.formatedConnections(response.data);
+    });
+  },
+  methods: {
+    formatedConnections(raw) {
+      raw.unmatched = [];
+      for (let gl_milestone of raw.milestones) {
+        let gradeMSid = raw.grade_milestones.findIndex(
+          (o) => o.id == gl_milestone.grade_milestone
+        );
+
+        if (gradeMSid == -1) {
+          raw.unmatched.push(gl_milestone);
+        } else {
+          if (raw.grade_milestones[gradeMSid].gl_milestones == undefined) {
+            raw.grade_milestones[gradeMSid].gl_milestones = [];
+          }
+          raw.grade_milestones[gradeMSid].gl_milestones.push(gl_milestone);
         }
+      }
+      return raw;
     },
-    methods: {
-        postFeedback() {
-            if (this.payload.feedback) {
-                Api.post('/feedback/', this.payload)
-                    .then(() => {
-                        this.$router.push({ name: 'groups' })
-                    })
-                    .catch(err => {
-                        this.error = err
-                        console.log(err)
-                    })
-            }
-        },
-    },
-}
+  },
+};
 </script>
 
 <style scoped>
-select,
-select option {
-    color: #dee2e6;
+.flexbox {
+  display: flex;
+  justify-content: space-between;
+  overflow: hidden;
+  flex-direction: column;
 }
 
-select:invalid,
-select option[value=""] {
-    color: #6c757d;
+.flexbox .board {
+  display: flex;
+  flex-direction: row;
+}
+.flexbox .board .card {
+  cursor: pointer;
 }
 </style>
