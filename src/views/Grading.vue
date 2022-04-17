e8a5bc824a957fa4898cdc159be15ebaa35fdbeb
Mart Kaasik <mart.kaasik@gmail.com>
2022-04-18 00:56:05 +0300

    #67 Edit node button

--- src/views/Grading.vue
+++ src/views/Grading.vue
@@ -63,7 +63,7 @@
           <div class="input-group-text rounded-0 mx-n1">to</div>
           <date-picker id="endtime" disabled v-model="payload.end" :config="options"></date-picker>
         </div>
-        <div class="row rounded m-0 bg-dark" v-if="selectedNode.data.id !== null">
+        <div class="row rounded m-0 bg-dark input-group" v-if="selectedNode.data.id !== 0">
           <div class="col border border-right-0 border-secondary rounded-left">
             Selected: &nbsp;
             <em class="font-weight-bold">{{ selectedNode.data.name, }}</em>
@@ -74,7 +74,14 @@
           </div>
 
           <!-- Delete Button -->
+
           <div class="input-group-append w-20">
+            <button
+              class="btn btn-outline-warning"
+              data-target="#editNodeModal"
+              data-toggle="modal"
+              type="button"
+            >Edit Node</button>
             <button
               class="btn btn-outline-danger rounded-r"
               data-target="#deleteNodeModal"
@@ -104,27 +111,21 @@
                   </button>
                 </div>
                 <div class="modal-body">
-                  <p>
-                  Are you quite sure you want to delete this node?
-                  </p>
+                  <p>Are you quite sure you want to delete this node?</p>
                   <p>Deleting a node from the tree will unlink all children and delete all user grades that are directly tied to that node.</p>
 
                   <div class="border rounded p-2">
-                    <span>
-                    Points worth: &nbsp;
-                    </span>
+                    <span>Points worth: &nbsp;</span>
                     <em class="font-weight-bold h4">{{ formatedPoints }}</em>
                     <br />
-                    <span v-if="selectedNode.data.description">
-                    Description: &nbsp;
-                    </span>
+                    <span v-if="selectedNode.data.description">Description: &nbsp;</span>
                     <em class="font-weight-bold">{{ selectedNode.data.description }}</em>
-
                   </div>
 
-                  <div v-if="selectedNode.data.subnodecount > 1" class="alert alert-danger my-3">This action will delete {{ selectedNode.data.subnodecount }} subnodes too!</div>
-
-
+                  <div
+                    v-if="selectedNode.data.subnodecount > 1"
+                    class="alert alert-danger my-3"
+                  >This action will delete {{ selectedNode.data.subnodecount }} subnodes too!</div>
                 </div>
                 <div class="modal-footer">
                   <button
@@ -138,6 +139,59 @@
             </div>
           </div>
 
+          <!-- Edit Modal -->
+          <div
+            class="modal fade"
+            id="editNodeModal"
+            tabindex="-1"
+            role="dialog"
+            aria-labelledby="editNodeModalLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5
+                    class="modal-title text-warning"
+                    id="editNodeModalLabel"
+                  >Edit Node {{ selectedNode.data.name }}</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      class="form-control mb-2"
+                      placeholder="Node name"
+                      v-model="selectedNode.data.name"
+                    />
+                    <input
+                      type="text"
+                      class="form-control mb-2"
+                      placeholder="Description"
+                      v-model="selectedNode.data.description"
+                    />
+                    <input
+                      type="number"
+                      class="form-control mb-2"
+                      placeholder="Points worth"
+                      v-model="selectedNode.data.total"
+                    />
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <button
+                    class="btn btn-outline-success rounded"
+                    data-dismiss="modal"
+                    type="button"
+                    @click="editNode"
+                  >Save</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -185,7 +239,7 @@ export default {
       },
       selectedNode: {
         id: null,
-        data: { name: "", id: null, total: 0, description: "", subnodecount: 0},
+        data: { name: "", id: null, total: 0, description: "", subnodecount: 0 },
       },
       graphData: null,
       addedNode: null,
@@ -246,6 +300,18 @@ export default {
           })
           .catch((err) => console.log(err));
       }
+    },
+    editNode() {
+      let payload = {
+        total: this.selectedNode.data.total,
+        name: this.selectedNode.data.name,
+        description: this.selectedNode.data.description,
+      }
+      Api.put("grade_category/" + this.selectedNode.id + "/", payload)
+        .then(() => {
+          this.getTree()
+        })
+        .catch((err) => console.log(err));
     },
     toggleMilestone() {
       let check = document.getElementById("ms_check").checked;
