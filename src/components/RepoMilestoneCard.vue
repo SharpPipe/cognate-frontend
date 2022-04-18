ea8891d1bec1df023b33a475155d590ed624fbef
Mart Kaasik <mart.kaasik@gmail.com>
2022-04-18 12:16:05 +0300

    #76 Show time spent on milestone

--- src/components/RepoMilestoneCard.vue
+++ src/components/RepoMilestoneCard.vue
@@ -9,12 +9,20 @@
                 v-if="!msData.graded"
                 class="badge badge-pill badge-warning"
             >Milestone {{ msData.milestone_id }}</div>
-            <div v-for="user in msData.user_points" :key="user.name">
-                <svg class="m-1" height="16" width="16">
-                    <circle cx="8" cy="8" r="8" :fill="`#${user.colour}`" />
-                </svg>
-                {{ (+user.points).toFixed(0) }}p
-            </div>
+            <table class="table table-borderless">
+                <tr v-for="user in msData.user_points" :key="user.name">
+                    <td class="p-0">
+                        <svg class="m-1" height="16" width="16">
+                            <circle cx="8" cy="8" r="8" :fill="`#${user.colour}`" />
+                        </svg>
+                    </td>
+                    <td class="p-0">{{ (+user.points).toFixed(0) }}p</td>
+                    <td class="p-0">
+                        <font-awesome-icon icon="fa-regular fa-clock" />
+                        {{ user.time_spent.toFixed(1) }}h
+                    </td>
+                </tr>
+            </table>
         </div>
     </div>
 </template>
