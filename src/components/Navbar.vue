c82b8e1c23bbbc4d9301430821fbc85a3c764167
Mart Kaasik <mart.kaasik@gmail.com>
2022-05-12 18:07:56 +0300

    navbar link to routerlink

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,7 +5,11 @@
               aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand font-italic font-weight-light" href="/">Cognate</a>
+      <router-link class="navbar-brand font-italic font-weight-light"
+                  :to="{ name: 'home'}" 
+      >
+      Cognate
+      </router-link>
 
       <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-1">
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  publicPath: '/services/cognate/front/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+}
