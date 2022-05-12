82f6374d6b863c771baee9d418d1e73e7b2df596
Mart Kaasik <mart.kaasik@gmail.com>
2022-05-12 18:33:43 +0300

    Taltech services path only in production

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: '/services/cognate/front/',
+  publicPath: process.env.VUE_APP_API_URL === '193.40.156.142'
+    ? '/'
+    : '/services/cognate/front',                    
   outputDir: 'dist',
   assetsDir: 'static',
 }
