0ee3887a2368aca395ba25895a22ef465193d380
mart <makaa@ttu.ee>
2022-05-22 21:14:04 +0300

    #99 Repo chart uses Bootstrap tooltips

--- src/components/visualizations/RepoChartMini.vue
+++ src/components/visualizations/RepoChartMini.vue
@@ -3,87 +3,99 @@
 </template>
 
 <script>
-import * as d3 from 'd3'
-const width = 320
-const height = 50
+import * as d3 from "d3";
+import $ from "jquery";
+
+const width = 320;
+const height = 50;
 
 export default {
   name: "GroupChartMini",
   props: ["ms_data", "k", "milestones"],
   data() {
     return {
-      width, height,
-    }
+      width,
+      height,
+    };
   },
   mounted() {
-    if (this.k == undefined) return
+    if (this.k == undefined) return;
 
-    this.makeChart()
+    this.makeChart();
   },
   methods: {
     makeChart() {
-      if (this.k == undefined) return
+      if (this.k == undefined) return;
 
-      let svg = d3.select("#repoms" + this.k)
+      let svg = d3.select("#repoms" + this.k);
 
-      let x = d3.scaleLinear()
-        .domain([0,6])
-        .range([20, width - 20])
-      
+      let x = d3
+        .scaleLinear()
+        .domain([0, 6])
+        .range([20, width - 20]);
+
       //let color = ["#343a40", "#dc3545", "#fd7e14", "#ffc107", "#28a745"]
       let c = (points) => {
-        if (points.reduce((x,y) => x+y, 0) === 0)
-          return "#dc3545"
-        if (points.includes(0)) 
-          return "#ffc107"
-        return "#28a745"
-      }
+        if (points.reduce((x, y) => x + y, 0) === 0) return "#dc3545";
+        if (points.includes(0)) return "#ffc107";
+        return "#28a745";
+      };
+
+      /*           <label
+            class="mb-1"
+            data-toggle="tooltip"
+            data-placement="top"
+            :title="point.description"
+            >{{ point.name }}</label
+          > */
 
       // Tooltip
+      $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+      });
+      /*
       let div = d3.select("body").append("div")
         .attr("class", "milestone-tooltip")
         .style("opacity", 0)
-      
+        */
+
       // Circles
-      svg.append("g")
+      svg
+        .append("g")
         .selectAll("circle")
         .data(this.milestones)
-        .enter().append("circle")
-        .attr("cx", (d, i)=> x(i))
-        .attr("cy", height/2)
+        .enter()
+        .append("circle")
+        .attr("data-toggle", "tooltip")
+        .attr("data-placement", "top")
+        .attr("data-html", true)
+        .attr("title", (d) => {
+          return (
+            "<b>Milestone " +
+            d.milestone_id +
+            "</b>" +
+            d.user_points
+              .map((u) => "<br/>" + u.name + ": " + Math.round(u.points))
+              .join("") 
+          );
+        })
+        .attr("cx", (d, i) => x(i))
+        .attr("cy", height / 2)
         .attr("r", 15)
-        .attr("fill", d => c(d.user_points.filter(u => !u.name.includes('project')).map(u => +u.points)))
+        .attr("fill", (d) =>
+          c(
+            d.user_points
+              .filter((u) => !u.name.includes("project"))
+              .map((u) => +u.points)
+          )
+        )
         .attr("stroke-width", 2)
-        .attr("stroke-opacity", 0.6)
-        .on("mouseover", (event, d) => {
-          var c = event.target
-          var matrix = c.getScreenCTM()
-            .translate(+c.getAttribute("cx"), +c.getAttribute("cy"))
-          div.transition()
-            .duration(100)
-            .style("opacity", 1);
-          let text = d.user_points.map(u => "<br/>" + u.name + ": " + Math.round(u.points)).join("")
-          let tooltip = div.html("<b>Milestone " + d.milestone_id + "</b>" + text + "<br/>")
-          let w = tooltip.node().getBoundingClientRect().width
-          tooltip.style("left", (window.pageXOffset + matrix.e - w / 2) + "px")
-                 .style("top", (window.pageYOffset + matrix.f - 40) + "px")
+        .attr("stroke-opacity", 0.6);
 
-
-          
-          d3.select(c).attr("stroke", "lightgray")
-        })
-        .on("mouseout", (event) => {
-          div.transition()
-            .duration(100)
-            .style("opacity", 0);
-          d3.select(event.target).attr("stroke", "")
-        })
-
-     return svg
-    }
-
+      return svg;
+    },
   },
-}
+};
 </script>
 
 <style>
