1906bae27571d42dac90cfe90eaebcbe44d6455d
mart <makaa@ttu.ee>
2022-05-29 20:43:49 +0300

    #111 Update profile view

--- src/views/BaseProfileSettings.vue
+++ src/views/BaseProfileSettings.vue
@@ -4,33 +4,65 @@
       <h2 class="text-info">{{ username }}</h2>
       <div class="row">
         <div class="col">
-          <form class="form-group" v-on:submit.prevent="submitToken">
-            <label for="exampleInputEmail1">Your Gitlab API Token</label>
-            <input
-              type="text"
-              class="form-control"
-              aria-describedby="tokenHelp"
-              placeholder="Access Token"
-              v-model="gitlab_token"
-            />
+          <div class="">
+            <label>Your Gitlab API Token</label>
+            <div class="input-group">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Access Token"
+                v-model="gitlab_token"
+              />
+              <div class="input-group-append">
+                <button
+                  class="btn btn-primary"
+                  type="button"
+                  @click="submitToken"
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
 
-            <small class="form-text text-muted float-right">
-              <a
-                href="https://gitlab.cs.ttu.ee/-/profile/personal_access_tokens"
-                class="alert-link"
-                target="_blank"
-                rel="noopener noreferrer"
-              >Generate a new Gitlab Token</a>
-              <!-- target and rel to open the link in new tab -->
-            </small>
-            <div class="form-group">
-              <button type="submit" class="btn btn-primary my-3">Submit</button>
+          <small class="form-text text-muted float-right">
+            <a
+              href="https://gitlab.cs.ttu.ee/-/profile/personal_access_tokens"
+              class="alert-link"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Generate a new Gitlab Token</a
+            >
+          </small>
+        </div>
+
+        <div class="col">
+            <label>Your identifier code (send it to your project manager)</label>
+          <form>
+            <div class="input-group">
+              <input
+                type="text"
+                class="form-control"
+                disabled
+                :value="userData.identifier"
+                placeholder="identifier"
+                id="copy-input"
+              />
+              <div class="input-group-append">
+                <button
+                  class="btn border"
+                  type="button"
+                  id="copy-button"
+                  @click="copyId"
+                  data-toggle="tooltip"
+                  data-placement="top"
+                  title="Copy to Clipboard"
+                >
+                  Copy
+                </button>
+              </div>
             </div>
           </form>
-        </div>
-        <div class="col">
-          <div class="alert alert-success" role="alert" v-if="success">{{ success }}</div>
-          <div class="alert alert-danger" role="alert" v-if="error">{{ error }}</div>
         </div>
       </div>
     </form>
@@ -39,32 +71,45 @@
 
 <script>
 import { Api } from "../axios-api";
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
+import $ from "jquery"
 
 export default {
-  name: 'BaseProfileSettings',
-  computed: mapState(['username']),
+  name: "BaseProfileSettings",
+  computed: mapState(["username"]),
   data() {
     return {
       gitlab_token: "",
-      success: "",
-      error: "",
-    }
+      userData: "",
+    };
   },
+  created() {
+    $('#copy-button').tooltip();
+    Api.get("/profile/")
+      .then((response) => {
+        this.userData = response.data.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip({trigger: 'hover'});
+  });
+},
   methods: {
     submitToken() {
-      Api.put('profile/', {
-        gitlab_token: this.gitlab_token
-      })
-        .then(response => {
-          this.success = "Great success!"
-          console.log(response.data)
-        })
-        .catch(err => {
-          this.error = err
-          console.log(err)
-        })
+      Api.put("profile/", { gitlab_token: this.gitlab_token }).catch((err) => {
+        console.log(err);
+      });
     },
+    copyId() {
+      navigator.clipboard.writeText(this.userData.identifier)
+      let btn = $('#copy-button')
+      btn.attr("title", "Copied!")
+      btn.attr("data-original-title", "Copied!")
+      btn.tooltip('update')
+    }
   },
-}
+};
 </script>
