16d845ba1160f96d23aad2e58d21b3f6656ea76c
mart <makaa@ttu.ee>
2022-05-30 20:06:31 +0300

    #114 Tooltip info

--- src/components/GroupAdminGeneral.vue
+++ src/components/GroupAdminGeneral.vue
@@ -1,42 +1,52 @@
 <template>
-        <div class="border rounded my-3 p-2">
-          <form class="form-group" v-on:submit.prevent="modifyGroup">
-            <label>Group name:</label>
-            <input
-              type="text"
-              class="form-control mb-3"
-              placeholder="Group name"
-              v-model="payload.name"
-            />
-            <label>Group description:</label>
-            <input
-              type="text"
-              class="form-control mb-3"
-              placeholder="Group description"
-              v-model="payload.description"
-            />
-            <label>GitLab Token:</label>
-            <input
-              type="text"
-              class="form-control mb-3"
-              placeholder="GitLab Token (optional)"
-              v-model="payload.gitlab_token"
-            />
+  <div class="border rounded my-3 p-2">
+    <form class="form-group" v-on:submit.prevent="modifyGroup">
+      <label>Group name</label>
+      <input
+        type="text"
+        class="form-control mb-3"
+        placeholder="Group name"
+        v-model="payload.name"
+      />
+      <label>Group description</label>
+      <input
+        type="text"
+        class="form-control mb-3"
+        placeholder="Group description"
+        v-model="payload.description"
+      />
+      <label
+        >GitLab Token
+        <label
+          class="mb-1"
+          data-toggle="tooltip"
+          data-placement="top"
+          data-html="true"
+          title="Cognate only needs <b>read-api</b> rights"
+        >
+          <font-awesome-icon icon="fa-solid fa-circle-info" />
+        </label>
+      </label>
+      <input
+        type="text"
+        class="form-control mb-3"
+        placeholder="GitLab Token (optional)"
+        v-model="payload.gitlab_token"
+      />
 
-            <div class="form-group">
-              <button type="submit" class="btn btn-primary my-2">
-                Update Group
-              </button>
-            </div>
-          </form>
-        </div>
+      <div class="form-group">
+        <button type="submit" class="btn btn-primary my-2">Update Group</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 import { Api } from "../axios-api";
+import $ from "jquery"
 
 export default {
-  name: 'GroupAdminGeneral',
+  name: "GroupAdminGeneral",
   data() {
     return {
       success: null,
@@ -46,7 +56,7 @@ export default {
         description: "",
         gitlab_token: "",
       },
-    }
+    };
   },
   created() {
     Api.get("/groups/")
@@ -61,6 +71,10 @@ export default {
         this.error = err;
         console.log(err);
       });
+
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip({ trigger: "hover" });
+    });
   },
   methods: {
     modifyGroup() {
@@ -74,6 +88,6 @@ export default {
           console.log(err);
         });
     },
-  }
-}
+  },
+};
 </script>
